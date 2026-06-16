05bc883f0de328377ae7bd23758d52a4bc01cbaf
Bruno Tochetto <draguisada@gmail.com>
2026-06-16 08:43:04 -0300

    não está mais rodando dps q coloquei o SqFlite

---
+++ armario_virtual/lib/database/db.dart
@@ -0,0 +1,26 @@
+
+// função auxiliar ASSÍNCRONA para PEGAR as infos do banco;
+// Também, a conexão do banco de dados;;
+// Future<Database> getDataBase() async {
+//   final String caminhoBanco = join(
+//     await getDatabasesPath(),
+//     'alunos.db',
+//   ); // Variável do endereço padrão do armazenamento do banco de dados no dispositivo
+//   return openDatabase(
+//     caminhoBanco,
+//     onCreate: (db, version) {
+//       // Só executa isso se não existe o banco;
+//       const String criacaoRoupa = 'CREATE TALE alunos'
+//       '(id INTEGER PRIMARY KEY AUTOINCREMENT'
+//       'foto BLOB'
+//       'ehfavorito INT'
+
+
+
+
+//       ;
+//       db.execute(criacaoRoupa, ); // Criação das tabelas
+//     },
+//     version: 1,
+//   );
+// }
--- armario_virtual/lib/main.dart
+++ armario_virtual/lib/main.dart
@@ -1,49 +1,71 @@
-import 'package:armario_virtual/src/widgets/botao_papelao.dart';
+import 'dart:io';
+
 import 'package:armario_virtual/src/widgets/decorators/container_foto.dart';
-import 'package:armario_virtual/src/widgets/testes.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import './src/theme/tema.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
+
+// import './database/db.dart';
 import 'src/page/inicial.dart';
 
 void main() {
-  runApp(MaterialApp(
-        home: Scaffold(
-          body: Center(
-            child: PaginaInicial()
-          ),
-          bottomNavigationBar: 
-            ContainerFoto(
-              image: 'fundo/fundoTextoAm.png',
-              height: 80,
-              boxFit: BoxFit.contain,
+  WidgetsFlutterBinding.ensureInitialized(); // <- Necessário na Web;
+  // Isso basicamente adiciona o JS na web
+  // também deve dar: dart run sqflite_common_ffi_web:setup
+  if (kIsWeb) {
+    databaseFactory = databaseFactoryFfiWeb;
+  } else if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    //padrão para qualquer app
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi; //usa essa biblioteca
+  } else {
+    //de outro
+    databaseFactory = databaseFactoryFfiWeb;
+  }
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(child: PaginaInicial()),
+        bottomNavigationBar: ContainerFoto(
+          image: 'fundo/fundoTextoAm.png',
+          height: 80,
+          boxFit: BoxFit.contain,
 
-              child: Expanded(
-                child: Center(child: Text("ola"))),
-              // child: BottomNavigationBar(
-
-              //   items: [
-              //     BottomNavigationBarItem(
-              //       icon: Image.asset(
-              //         'assets/icon/iconArmario.png',
-              //         height: 50,
-              //         ),
-              //       label: "Armario"
-              //     ),
-              //     BottomNavigationBarItem(
-              //       icon: Image.asset(
-              //         'assets/icon/iconCasa.png',
-              //         height: 50,
-              //       ),
-              //       label: "Armario",
-              
-              
-              //     ),
-              //   ],
-                
-              //           ),
+          child: Expanded(
+            // child: Center(child: Text("ola"))),
+            child: Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Image.asset('assets/icon/iconArmario.png'),
+                ),
+              ],
             ),
+
+            // child: BottomNavigationBar(
+
+            //   items: [
+            //     BottomNavigationBarItem(
+            //       icon: Image.asset(
+            //         'assets/icon/iconArmario.png',
+            //         height: 50,
+            //         ),
+            //       label: "Armario"
+            //     ),
+            //     BottomNavigationBarItem(
+            //       icon: Image.asset(
+            //         'assets/icon/iconCasa.png',
+            //         height: 50,
+            //       ),
+            //       label: "Armario",
+
+            //     ),
+            //   ],
+          ),
+        ),
       ),
       // theme: temaApp
-  )
-    ); // Função que chama o FLUTTER
+    ),
+  ); // Função que chama o FLUTTER
 }
